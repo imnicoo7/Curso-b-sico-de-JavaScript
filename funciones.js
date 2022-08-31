e71aaaa862d4da9bc37c43687bbed516f64901bc
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-31 12:11:18 -0500

    curso js básico

---
+++ funciones.js
@@ -0,0 +1,11 @@
+//declarativas
+function suma(a,b) {
+    console.log(a + b);
+}
+
+suma(2,3);
+
+//expresión
+let miFuncion = function(a,b) {
+    return a + b;
+}
---
+++ valores.js
@@ -0,0 +1,16 @@
+//primitivos
+40
+'hola soy un txt'
+true 
+false 
+null
+undefined
+
+//no primitivos o objetos
+
+[1,2,3]
+{
+    nombre: 'Nicolas';
+    edad: 14;
+    curso: 'I-S'
+}
