ad9451ed0a12187c0884dbb2c59a7e4f7b644cb1
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-09-01 10:36:36 -0500

    operadores

--- 8-operadores-comparacion-aritmeticos.js
+++ 8-operadores-comparacion-aritmeticos.js
@@ -1,10 +1,10 @@
 //Operadores binarios:
-3 + 2 //Suma
-50 - 10 // Resta
-10 * 20 //Multiplicación
-20 / 2 //División
+3 + 2; //Suma
+50 - 10; // Resta
+10 * 20; //Multiplicación
+20 / 2; //División
 
-"Diego " + "De Granda" //concatenación de strings
+"Diego " + "De Granda"; //concatenación de strings
 
 //Operadores unitarios:
 !false //negación de la negación = true
@@ -17,15 +17,15 @@ var a = 1; //Asignamos un valor a la variable
 
 3 === "3"; //Compara y valida los tipos y valores. Devuelve "falso" en este caso
 
-5 < 3 //Compara y valida si el 5 es menor a 3
-5 > 3 //Compara y valida si el 5 es mayor a 3
-5 <= 6 //Compara y valida si el 5 es menor o igual al 6
-5 >= 6 //Compara y valida si el 5 es mayor o igual al 6
+5 < 3; //Compara y valida si el 5 es menor a 3
+5 > 3; //Compara y valida si el 5 es mayor a 3
+5 <= 6; //Compara y valida si el 5 es menor o igual al 6
+5 >= 6; //Compara y valida si el 5 es mayor o igual al 6
 
-a && b //Valida si ambas variables son verdaderas para que se cumpla la condición
-a || b //Aquí se cumple la condición si alguna de las dos variables es verdadera
+a && b; //Valida si ambas variables son verdaderas para que se cumpla la condición
+a || b; //Aquí se cumple la condición si alguna de las dos variables es verdadera
 
-var edad = 40
-edad++ //Incrementa el valor en 1
+var edad = 40;
+edad++; //Incrementa el valor en 1
 
-edad += 2 //Incrementa el valor por 2
+edad += 2; //Incrementa el valor por 2
